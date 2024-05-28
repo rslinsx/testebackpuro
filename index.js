c0f3bff990c3545395587aa4f388827a049cebe4
Rafael Lins <rslinsx@gmail.com>
2024-05-28 13:57:29 -0300

    oi

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/', (req, res)=>{
     res.send('Hello, world!');
 });
 
+app.get('/teste', (req, res)=>{
+    res.send('Aqui é uma página testeeeeeeeeeeeeeeeeeeeeeeeeeeee');
+});
+
+
 app.listen(PORT, ()=>{
     console.log(`Rodando na porta ${PORT}`);
 });
