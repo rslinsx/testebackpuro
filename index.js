4317d22aa3a1235679115cbc1554aeb224c76291
Rafael Lins <rslinsx@gmail.com>
2024-05-28 13:44:21 -0300

    port rail

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 8081;
+const PORT = process.env.PORT || 8081;
 
 app.get('/', (req, res)=>{
     res.send('Hello, world!');
